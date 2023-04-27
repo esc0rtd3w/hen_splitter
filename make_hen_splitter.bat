a726da50aa263c13b3787596a8e685c4ac851894
PS3Xploit Team <esc0rtd3w@gmail.com>
2023-04-26 22:02:47 -0400

    Updated text

--- make_hen_splitter.bat
+++ make_hen_splitter.bat
@@ -1,5 +1,11 @@
 @echo off
+cls
+echo Building Hen Splitter EXE...
+echo.
 set VSDIR="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC"
 call %VSDIR%\vcvarsall.bat x86
 nmake -f Makefile
+echo.
+echo done.
+echo.
 pause
